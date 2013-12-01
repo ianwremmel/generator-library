f137cc0afbe76913a06850842e2f2e07bbb97f53
Ian Remmel <design@ianwremmel.com>
2013-12-01 12:15:37 -0500

    updated basic tests

--- test/test-creation.js
+++ test/test-creation.js
@@ -6,33 +6,41 @@ var helpers = require('yeoman-generator').test;
 
 
 describe('library generator', function () {
-    beforeEach(function (done) {
-        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
-            if (err) {
-                return done(err);
-            }
+  beforeEach(function (done) {
+    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+      if (err) {
+        return done(err);
+      }
 
-            this.app = helpers.createGenerator('library:app', [
-                '../../app'
-            ]);
-            done();
-        }.bind(this));
+      this.app = helpers.createGenerator('library:app', [
+        '../../app'
+      ]);
+      done();
+    }.bind(this));
+  });
+
+  it('creates expected files', function (done) {
+    var expected = [
+      // add files you expect to exist here.
+      '.jshintrc',
+      '.editorconfig',
+      '.gitignore',
+      'LICENSE',
+      'Gruntfile.coffee',
+      'bower.json',
+      'package.json',
+      'test/test.coffee',
+      'src/index.js'
+    ];
+
+    helpers.mockPrompt(this.app, {
+      'enableTests': true,
+      'enableBrowserSupport': true
     });
-
-    it('creates expected files', function (done) {
-        var expected = [
-            // add files you expect to exist here.
-            '.jshintrc',
-            '.editorconfig'
-        ];
-
-        helpers.mockPrompt(this.app, {
-            'someOption': true
-        });
-        this.app.options['skip-install'] = true;
-        this.app.run({}, function () {
-            helpers.assertFiles(expected);
-            done();
-        });
+    this.app.options['skip-install'] = true;
+    this.app.run({}, function () {
+      helpers.assertFiles(expected);
+      done();
     });
+  });
 });
